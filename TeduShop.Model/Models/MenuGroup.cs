a9b07b611f86bc3b445cffffb568e5968e88a734
donpham10 <donpham10@gmail.com>
2020-05-15 22:27:18 +0700

    add MenuGroup, Order, OrderDetail

--- TeduShop.Model/Models/MenuGroup.cs
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
+
 namespace TeduShop.Model.Models
 {
     [Table("MenuGroups")]
---
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,11 @@
+namespace TeduShop.Model.Models
+{
+    public class OrderDetail
+    {
+       public int OrderID {set; get;}
+
+       public int ProductID {set; get;}
+       public int Quantity {set; get;}
+       
+    }
+}
