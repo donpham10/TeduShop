8bfdba6c58623368b78966d4bc99d49b213d565a
donpham10 <donpham10@gmail.com>
2020-05-27 12:06:01 +0700

    format code

--- TeduShop.Model/Models/Menu.cs
+++ TeduShop.Model/Models/Menu.cs
@@ -4,23 +4,29 @@ namespace TeduShop.Model.Models {
     [Table (" Menus")]
     public class Menu {
         [Key]
-        
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [MaxLength(50)]
+
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+        [MaxLength (50)]
         public int ID { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
-        [MaxLength(256)]
+        [MaxLength (256)]
         public string URL { get; set; }
 
         public int? DisplayOrder { get; set; }
+
         [Required]
         public int GroupID { set; get; }
-        [ForeignKey("GroupID")]
+
+        [ForeignKey ("GroupID")]
         public virtual MenuGroup MenuGroup { get; set; }
-        [MaxLength(10)]
+
+        [MaxLength (10)]
         public string Target { get; set; }
+
         [Required]
         public bool Status { get; set; }
 
--- TeduShop.Model/Models/SystempConfig.cs
+++ TeduShop.Model/Models/SystempConfig.cs
@@ -6,18 +6,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeduShop.Model.Models
-{
-    [Table("SystempConfigs")]
-    public class SystempConfig
-    {
+namespace TeduShop.Model.Models {
+    [Table ("SystempConfigs")]
+    public class SystempConfig {
         [Key]
         public int Id { get; set; }
+
         [Required]
-        [Column(TypeName ="varchar")]
-        [MaxLength(50)]
+        [Column (TypeName = "varchar")]
+        [MaxLength (50)]
         public string Code { set; get; }
-        [MaxLength(50)]
+
+        [MaxLength (50)]
         public string ValueString { set; get; }
         public int? ValueInt { set; get; }
     }
